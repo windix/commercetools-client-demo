36550cac258909d1bdb909998fe92c215635595a
Wei Feng <wei.feng@kmart.com.au>
2020-10-20 11:05:01 +1100

    more lib

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  overrides: [{ files: "*.yml", options: { singleQuote: false } }],
+  singleQuote: true,
+  trailingComma: "all",
+  semi: false,
+  printWidth: 100,
+  htmlWhitespaceSensitivity: 'ignore'
+}
